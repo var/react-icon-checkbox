45a6d529a4a9b92caf534fce4b1c402fc45cad7d
Vimal Atreya Ramaka <vimal@ramaka.com>
2018-08-21 14:56:02 -0400

    init

---
+++ src/IconCheckbox.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+class IconCheckbox extends Component {
+    render() {
+        return (
+            <label className='icon-checkbox-container'>
+            <input className='icon-checkbox' type='checkbox' onClick={this.props.onClick} checked={this.props.checked} />
+            <span className={this.props.iconContainerClassName ? this.props.iconContainerClassName : 'icon-container'} style={this.props.iconContainerStyle}>{this.props.checked ? this.props.checkedIcon : this.props.uncheckedIcon}</span>
+            </label>
+        )
+    }
+}
+
+IconCheckbox.defaultProps = {
+    checked: false
+}
+
+IconCheckbox.propTypes = {
+    checked: PropTypes.bool,
+    onClick: PropTypes.func,
+    iconContainerClassName: PropTypes.string,
+    iconContainerStyle: PropTypes.style,
+    checkedIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.func, PropTypes.element]),
+    uncheckedIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.func, PropTypes.element]),
+}
+
+export default IconCheckbox;
